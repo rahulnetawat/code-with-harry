db629d974b7b37d3c2061bef50f7b37c9a34df5a
Rahul Netawat <rahul.netawat@webdunia.net>
2018-12-05 18:03:27 +0530

    asdfasdf

--- absolute-beginners/tut1.py
+++ absolute-beginners/tut1.py
@@ -1,3 +1,33 @@
 # First tutorial
 # Date: 05-December-2018
+# Comments, Escape Sequence and Print Statements.
+
+# This is first comment example.
 print("Hello World !!!!")
+
+'''
+Multi line comment example. 
+With triple qoute we can comment multiple lines.
+'''
+
+# end is merging last print line.
+print("This is first print.", end="")
+print("This is second print.")
+
+# end with space.
+print("This is first print.", end=" ")
+print("This is second print.")
+
+#end with comma
+print("This is first print", end=",")
+print("This is second print.")
+
+#New line print.
+print("C:\Rahul")
+
+# Now Rahul is going to print in new line. but we need to escape the new line.
+print("C:\nRahul")
+
+# By using '\'(Backslash) we are going to escape the new line.
+# By the way \n is a new line escape sequence.
+print("C:\\Rahul")

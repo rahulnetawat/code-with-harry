358d66878dc753915315b933c33e4f934409b2c2
Rahul Netawat <rahul.netawat@webdunia.net>
2018-12-05 20:07:25 +0530

    asdfasd

---
+++ absolute-beginners/tut2.py
@@ -0,0 +1,24 @@
+var1 = "Hello World !!" # String Variable
+var2 = "4"                # Integer Variable
+var3 = 38.2             # Float Variable
+var4 = "Rahul"
+var5 = "10"
+
+# print(var1)
+# print(type(var1))
+# print(var1 + var2)
+# print(var1 + var4)
+# print(var1 + var4)
+# print(var2 + var3)
+# print(var2+var5) # now it is concatenating
+# print(int(var2) + int(var5)) # Type casting example.
+
+'''
+str()
+int()
+float()
+'''
+
+# print(10 * "Hello World !!!\n") # Here we can print Hello World string 10 times.
+print(100 * int(var5) + int(var2)) # Here we can multiply the value.
+# print(100 * str(int(var5) + int(var2))  # Here we are converting int in string after adding.)

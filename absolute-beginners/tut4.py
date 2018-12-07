48fe848aec3129dc358e215f256bead272b8d5da
Rahul Netawat <rahul.netawat@webdunia.net>
2018-12-07 18:43:27 +0530

    tut4 file.

---
+++ absolute-beginners/tut4.py
@@ -0,0 +1,13 @@
+# Lists and Lists Functions.
+laptop = ["ram", "battery", "hdd", "led", "wifi", "mouse", 227]
+
+# print(laptop[6])
+
+numbers = [2, 8, 9, 11, 67, 7]
+# print(numbers[4])
+
+# numbers.sort()    # Sort and reverse may change the original list parameter. Here we are using numbers name list.
+# numbers.reverse()
+
+print(numbers[1:4]) # We are getting output in list. Slicing also working here.
+

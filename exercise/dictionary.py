f274974d96519c3eac62ad562a1ea7bedd324a86
Rahul Netawat <rahul.netawat@webdunia.net>
2018-12-10 18:28:20 +0530

    exercise folder with file.

---
+++ exercise/dictionary.py
@@ -0,0 +1,10 @@
+myword = {"Rahul": "DevOps Engineer",
+        "Rohit": "Examiner",
+        "Dheeraj": "Gym Trainer",
+        "Chetan": "Jhabua Power Plant"
+        }
+print(myword.keys())
+print("Check Keys name from above line.")
+print("Enter your choice name:", end='')
+tia1 = input()
+print(myword[tia1])

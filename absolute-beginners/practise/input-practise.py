95564e154a18ade4e9df80e477e29f488d22cd8f
Rahul Netawat <rahul.netawat@webdunia.net>
2019-04-15 01:27:25 +0530

    new practise folder

---
+++ absolute-beginners/practise/input-practise.py
@@ -0,0 +1,17 @@
+# print("Please Enter your number")
+#
+# inpnum = input()
+#
+# # Below statement to print in String
+# # print("You Entered the number", inpnum + str(10))
+#
+# # Below statement to Print in Integer
+# print("You Entered the number", int(inpnum) + 10)
+
+print("Please Enter your First Number: ")
+val1 = input()
+
+print("Please Enter your Second Number: ")
+val2 = input()
+
+print("Sum :", int(val1)+int(val2))

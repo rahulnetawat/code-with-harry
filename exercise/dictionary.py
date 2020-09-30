cdbb0261c0c7902a7be585aaa20a074026523825
shubhambhawsar-5782 <51023390+shubhambhawsar-5782@users.noreply.github.com>
2020-09-30 22:46:13 +0530

    Update dictionary.py

--- exercise/dictionary.py
+++ exercise/dictionary.py
@@ -1,10 +1,14 @@
+#Define the Dictionary
 myword = {"centos": "Open source server distribution.",
         "ubuntu": "Open source desktop distribution",
         "redhat": "Propriatory OS.",
         "arch": "Open Source desktop and client distribution."
         }
+#print all the keys of Dictionary
 print(myword.keys())
 print("Check Keys name from above line.")
 print("Enter your choice name:", end='')
+#To take an input from the user
 tia1 = input()
+#print the Dictionary Value according to the keys as an input
 print(myword[tia1])

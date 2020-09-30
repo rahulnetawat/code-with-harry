2c673f43f3bd2f9243a89b8fd5f6b9dea4130cea
GauravMS11 <72162639+GauravMS11@users.noreply.github.com>
2020-10-01 00:30:39 +0530

    Add Document to dictionery.py
    it is always good practice that to write comments in code.

--- exercise/dictionary.py
+++ exercise/dictionary.py
@@ -1,10 +1,13 @@
+#Creating Dictionery
 myword = {"centos": "Open source server distribution.",
         "ubuntu": "Open source desktop distribution",
         "redhat": "Propriatory OS.",
         "arch": "Open Source desktop and client distribution."
         }
+#Printing all keys of dictionery
 print(myword.keys())
 print("Check Keys name from above line.")
 print("Enter your choice name:", end='')
+#take input from user to print element of dictionery.
 tia1 = input()
 print(myword[tia1])

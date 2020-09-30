f32356fff9d687429f8dd74db3203ddc5af74226
GauravMS11 <72162639+GauravMS11@users.noreply.github.com>
2020-10-01 00:43:15 +0530

    Use of get function in dictionery.py
    when the user enters the key but the key is not in dictionary, at that time we will show the default message to the user by "get" function.

--- exercise/dictionary.py
+++ exercise/dictionary.py
@@ -10,4 +10,6 @@ print("Check Keys name from above line.")
 print("Enter your choice name:", end='')
 #take input from user to print element of dictionery.
 tia1 = input()
-print(myword[tia1])
+#use of get functio to access element of dictionery.
+print(myword.get(tia1,"No Such Key exist in dictionery"))
+#print(myword[tia1])
